6c2c553d9e78bd46e2498f48dd5dd3468e6af8a3
youjung95 <youjung94111@gmail.com>
2024-10-17 16:08:01 +0900

    1

---
+++ data.js
@@ -0,0 +1,269 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "버진로드뷰",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6854361358262198,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.07025384264244572,
+          "pitch": -0.00031151284382602284,
+          "rotation": 12.566370614359176,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1641312017756977,
+          "pitch": 0.2930120683135762,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">결혼</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.45201271696152645,
+          "pitch": -0.3193796114972045,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">웨딩홀&nbsp;</font></font></font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">버진로드 뷰&nbsp;</font></font></font></font><div><br><div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">창문을 배치함으로써 디자인적인 느낌과 개방감을 느낄 수 있으며, 금색 몰딩&nbsp;</font></font></font></font><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"> 연한 하늘색 지형 버진로드 더 해 화려한 아름다운 느낌을 더함</font></font></div></div>"
+        }
+      ]
+    },
+    {
+      "id": "1--",
+      "name": "중앙뷰로 이동",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2559146732004205,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "2-1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.44901493607292053,
+          "pitch": 0.09207581915082841,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">웨딩홀</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">주례석</font></font><div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">&nbsp;</font></font></div><div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">앞쪽은 신전느낌의 조형물과 분수대를 배치하여 성스럽고 아름다운 느낌을 주었으며&nbsp; 양측에 아팝나무(영원한 사랑)를 배치하였고</font></font></div><div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">뒤쪽은 신부입장 계단 배치와 화분 그리고&nbsp; 캐노피 조형물을 두어 더욱 성스러운 느낌을 강조하였음</font></font></div><div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><br></font></font></div>"
+        }
+      ]
+    },
+    {
+      "id": "2-1--",
+      "name": "하객뷰1 로 이동",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0036316840000072403,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "3-2--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.34728476929078766,
+          "pitch": -0.11433962612228044,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">하객석</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">사각커튼월을 사용하여 빛의 투영을보여주며 유리 테이블과 투명플라스틱의자를 배치하였으며&nbsp;</font></font><div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">금색 티타늄 몰딩과 캐노피 조형물그리고 샹들리에로 아름다움이 보여짐</font></font></div>"
+        }
+      ]
+    },
+    {
+      "id": "3-2--",
+      "name": "하객뷰2 로 이동",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5734627393011866,
+          "pitch": 0,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">하객석</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">어두운 느낌과 조명을 더해 표현하였음</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "입구",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.01943484715031829,
+          "pitch": 0.026635601446924895,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6381510655395566,
+          "pitch": -0.14871333074164816,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">예식장 입구</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">금장 티타늄 몰딩과 아치형 창문 연한 하늘색 스타코 외장 마감을 사용하였으며 금장 티아라 느낌의&nbsp; 장식을 활용하여 아름다고 화려한 느낌을 줌&nbsp;</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "조경",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0007677352999788667,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.582368020851316,
+          "pitch": -0.46499651397517283,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">조경</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">입구 앞쪽으로 사람들이 쉬거나 구경할 수 있는 꽃과 나무를 심어 벤치 에서 분수대를와 자연을 보며 쉴수있는 작은 쉼터를 만들어봄</font></font>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
